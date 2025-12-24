b9b3b0ed5923e80bcc4b51403e3b2ecf821195f1
SergeiVA <anikeevvlg@yandex.ru>
2025-12-24 18:49:20 +0300

    ocker first commit

---
+++ docker/postgres/data_1.sql
@@ -0,0 +1,19 @@
+---- Сначала очищаем таблицы с внешними ключами (дочерние)
+--DELETE FROM booking_service;
+--DELETE FROM report_attachments;
+--DELETE FROM notifications;
+--DELETE FROM reports;
+--DELETE FROM bookings;
+--DELETE FROM user_type_job_type;
+--
+---- Затем очищаем родительские таблицы
+--DELETE FROM services;
+--DELETE FROM job_types;
+--DELETE FROM rooms;
+--DELETE FROM promo_codes;
+--DELETE FROM users;
+--DELETE FROM user_types;
+--
+--
+--
+
