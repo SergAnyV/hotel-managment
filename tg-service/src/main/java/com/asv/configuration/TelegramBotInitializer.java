1b98eb5b5ffe4915a1f73e62e5c90f1b21b1f69a
sergei <anikeevvlg@yandex.ru>
2026-01-18 18:38:35 +0300

    added Retryable to TG Bot

--- tg-service/src/main/java/com/asv/configuration/TelegramBotInitializer.java
+++ tg-service/src/main/java/com/asv/configuration/TelegramBotInitializer.java
@@ -12,35 +12,6 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-//
-//@Slf4j
-//@Component
-//@RequiredArgsConstructor
-//public class TelegramBotInitializer {
-//
-//    private final HotelClientTB hotelClientTB;
-//
-//    @PostConstruct
-//    public void registerBot() throws TelegramApiException {
-//        tryRegisterBot();
-//    }
-//
-//    @Retryable(
-//            value = TelegramApiException.class,
-//            maxAttempts = 10,
-//            backoff = @Backoff(delay = 3_000)
-//    )
-//    public void tryRegisterBot() throws TelegramApiException {
-//        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-//        botsApi.registerBot(hotelClientTB);
-//        log.info("Telegram-бот успешно запущен");
-//    }
-//
-//    @Recover
-//    public void recover(TelegramApiException e) {
-//        log.warn("Превышено количество попыток запуска бота. Ошибка: {}", e.getMessage());
-//    }
-//}
 
 @Slf4j
 @Component
